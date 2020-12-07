dc45542cdd2a28755d18ea6e3d7f52c4b9b217be
Yatikabhardwaj <yatikab17@gmail.com>
2020-12-07 15:58:40 +0530

    created sign-up

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ mongoose.connect('mongodb://localhost/codeial_development');
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+db.on('error', console.error.bind(console, "888888888888888888888888888888888Error connecting to MongoDB"));
 
 db.once('open',function(){
     console.log('Connected to Database :: MongoDB');
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,59 @@
+
+const User = require('../models/user');
+
+
 module.exports.profile= function (req,res){
     return res.render('user_profile',{
         title: "User Profile"
     });
 }
 
+//renders the sign up page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | sign up"
+    })
+}
+
+//renders the sign in page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codeial | sign in"
+    })
+}
+
 module.exports.posts = function(req,res){
     res.end('<h1>User Posts</h1>');
+}
+
+//get sign up data
+module.exports.create = function(req,res){
+    console.log('HelloHello');
+    if(req.body.password != req.body.confirm_password){
+         return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+    if(err){
+        console.log('Error in finding user in signing up');
+        return;
+    }
+    if(!user){
+        User.create(req.body,function(err,user){
+            if(err){
+                console.log('Error in creating user while signing up');
+                return;
+            }
+            return res.redirect('/users/sign-in');
+        
+    })
+   }
+   else{
+       return res.redirect('back');
+   }
+ });
+}
+
+//get sign in data and create session for user
+module.exports.createSession = function(req,res){
+
 }
